3325815fb6d2d8334f6c499ddbf624cf76875c11
gderuwez <gderuwez@gmail.com>
2018-08-29 10:20:20 +0200

    database export

--- database/cgpi.sql
+++ database/cgpi.sql
@@ -2,13 +2,21 @@
 -- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Aug 28, 2018 at 02:26 PM
--- Server version: 5.7.21
--- PHP Version: 7.2.7
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2018 at 10:19 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cgpi`
@@ -20,11 +28,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `devis`
 --
 
-CREATE TABLE `devis` (
-  `iddevis` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `devis` (
+  `iddevis` int(11) NOT NULL AUTO_INCREMENT,
   `estimationtime` int(11) NOT NULL,
   `estimationprice` int(11) NOT NULL,
-  `datedevis` date NOT NULL
+  `datedevis` date NOT NULL,
+  PRIMARY KEY (`iddevis`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -33,13 +42,16 @@ CREATE TABLE `devis` (
 -- Table structure for table `factures`
 --
 
-CREATE TABLE `factures` (
-  `idfactures` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `factures` (
+  `idfactures` int(11) NOT NULL AUTO_INCREMENT,
   `datefacture` date NOT NULL,
   `prestationmotif` varchar(45) NOT NULL,
   `idsociete` int(11) NOT NULL,
-  `idpersonnes` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `idpersonnes` int(11) NOT NULL,
+  PRIMARY KEY (`idfactures`),
+  KEY `factures_ibfk_1` (`idsociete`),
+  KEY `idpersonnes` (`idpersonnes`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `factures`
@@ -67,11 +79,13 @@ INSERT INTO `factures` (`idfactures`, `datefacture`, `prestationmotif`, `idsocie
 -- Table structure for table `notecredit`
 --
 
-CREATE TABLE `notecredit` (
-  `idnotecredit` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `notecredit` (
+  `idnotecredit` int(11) NOT NULL AUTO_INCREMENT,
   `reduction` int(11) NOT NULL,
   `datenotecredit` date NOT NULL,
-  `idfactures` int(11) NOT NULL
+  `idfactures` int(11) NOT NULL,
+  PRIMARY KEY (`idnotecredit`),
+  KEY `notecredit_ibfk_1` (`idfactures`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -80,14 +94,16 @@ CREATE TABLE `notecredit` (
 -- Table structure for table `personnes`
 --
 
-CREATE TABLE `personnes` (
-  `idpersonnes` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `personnes` (
+  `idpersonnes` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `personnesphone` int(20) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `idsociete` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `idsociete` int(11) NOT NULL,
+  PRIMARY KEY (`idpersonnes`),
+  KEY `personnes_ibfk_1` (`idsociete`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `personnes`
@@ -112,14 +128,15 @@ INSERT INTO `personnes` (`idpersonnes`, `name`, `firstname`, `personnesphone`, `
 -- Table structure for table `societe`
 --
 
-CREATE TABLE `societe` (
-  `idsociete` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `societe` (
+  `idsociete` int(11) NOT NULL AUTO_INCREMENT,
   `socialname` varchar(45) NOT NULL,
   `adresse` varchar(45) NOT NULL,
   `country` varchar(45) NOT NULL,
   `tvanumber` int(3) NOT NULL,
-  `telephonesociete` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `telephonesociete` int(20) NOT NULL,
+  PRIMARY KEY (`idsociete`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `societe`
@@ -139,97 +156,14 @@ INSERT INTO `societe` (`idsociete`, `socialname`, `adresse`, `country`, `tvanumb
 -- Table structure for table `type`
 --
 
-CREATE TABLE `type` (
-  `idtype` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `type` (
+  `idtype` int(11) NOT NULL AUTO_INCREMENT,
   `type` set('ASBL','SPRL','SA') NOT NULL,
   `relation` set('fournisseur','client') NOT NULL,
-  `idsociete` int(11) NOT NULL
+  `idsociete` int(11) NOT NULL,
+  PRIMARY KEY (`idtype`),
+  KEY `idsociete` (`idsociete`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `devis`
---
-ALTER TABLE `devis`
-  ADD PRIMARY KEY (`iddevis`);
-
---
--- Indexes for table `factures`
---
-ALTER TABLE `factures`
-  ADD PRIMARY KEY (`idfactures`),
-  ADD KEY `factures_ibfk_1` (`idsociete`),
-  ADD KEY `idpersonnes` (`idpersonnes`);
-
---
--- Indexes for table `notecredit`
---
-ALTER TABLE `notecredit`
-  ADD PRIMARY KEY (`idnotecredit`),
-  ADD KEY `notecredit_ibfk_1` (`idfactures`);
-
---
--- Indexes for table `personnes`
---
-ALTER TABLE `personnes`
-  ADD PRIMARY KEY (`idpersonnes`),
-  ADD KEY `personnes_ibfk_1` (`idsociete`);
-
---
--- Indexes for table `societe`
---
-ALTER TABLE `societe`
-  ADD PRIMARY KEY (`idsociete`);
-
---
--- Indexes for table `type`
---
-ALTER TABLE `type`
-  ADD PRIMARY KEY (`idtype`),
-  ADD KEY `idsociete` (`idsociete`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `devis`
---
-ALTER TABLE `devis`
-  MODIFY `iddevis` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `factures`
---
-ALTER TABLE `factures`
-  MODIFY `idfactures` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `notecredit`
---
-ALTER TABLE `notecredit`
-  MODIFY `idnotecredit` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `personnes`
---
-ALTER TABLE `personnes`
-  MODIFY `idpersonnes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `societe`
---
-ALTER TABLE `societe`
-  MODIFY `idsociete` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `type`
---
-ALTER TABLE `type`
-  MODIFY `idtype` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -259,3 +193,8 @@ ALTER TABLE `personnes`
 --
 ALTER TABLE `type`
   ADD CONSTRAINT `type_ibfk_1` FOREIGN KEY (`idsociete`) REFERENCES `societe` (`idsociete`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
