63a0103fc05e84eb8bcfe70ce228f191b4418242
gderuwez <gderuwez@gmail.com>
2018-08-30 12:12:01 +0200

    prepare for mvc

--- display.php
+++ display.php
@@ -1,5 +1,6 @@
 <?php
 
+//common model
 /* Connect to a MySQL database using driver invocation */
 $dsn = getenv('DSN');
 $user = getenv('USER');
@@ -11,25 +12,27 @@ function testselect($par1, $par2) {
   }
 }
 
-
 try {
     $dbh = new PDO($dsn, $user, $password);
+
+    //dashboard model
     $displayLatestFactures = $dbh->query('SELECT * FROM factures LEFT JOIN societe ON factures.idsociete = societe.idsociete ORDER BY datefacture DESC LIMIT 0,5');
     $displayLatestCredit = $dbh->query('SELECT * FROM notecredit ORDER BY datenotecredit DESC LIMIT 0,5');
     $displayLatestPeople = $dbh->query('SELECT * FROM personnes LEFT JOIN societe ON personnes.idsociete = societe.idsociete ORDER BY idpersonnes DESC LIMIT 0,5');
     $displayLatestSocieties = $dbh->query('SELECT * FROM societe ORDER BY idsociete DESC LIMIT 0,5');
 
+    //societe model
     $displaySocietiesAlphab = $dbh->query('SELECT socialname, idsociete FROM societe ORDER BY socialname ASC');
+    //factures model
     $displayFacturesAlphab = $dbh->query('SELECT idfactures, datefacture FROM factures ORDER BY datefacture DESC');
+    //annuaire model
     $displayAnnuaireAlphab = $dbh->query('SELECT name, firstname, idpersonnes FROM personnes ORDER BY name ASC');
-
-    $displayDetailsSocieties = $dbh->prepare('SELECT societe.socialstatus, societe.adresse, societe.telephonesociete, societe.tvanumber, societe.account, devis.iddevis, boncommande.idboncommande, factures.idfactures, notecredit.idnotecredit, personnes.name, personnes.firstname, type.type FROM societe left JOIN factures ON societe.idsociete = factures.idfactures LEFT JOIN personnes ON societe.idsociete = personnes.idsociete LEFT JOIN notecredit ON factures.idfactures = notecredit.idfactures LEFT JOIN type ON societe.idsociete = type.idsociete LEFT JOIN boncommande ON factures.idfactures = boncommande.idfactures LEFT JOIN devis ON boncommande.idboncommande = devis.idboncommande WHERE societe.idsociete = :id');
+    //fournisseurs model
     $displaySocietiesSuppliers = $dbh->query("SELECT socialname FROM societe LEFT JOIN type ON societe.idsociete = type.idsociete WHERE type.relation = 'fournisseur'");
-
+    //clients model
     $displaySocietiesCustomers = $dbh->query("SELECT socialname FROM societe LEFT JOIN type ON societe.idsociete = type.idsociete WHERE type.relation = 'client'");
 
-    // $displayDetailsSocieties = $dbh->prepare('SELECT societe.socialname, societe.adresse, societe.telephonesociete, societe.tvanumber, factures.idfactures, personnes.name, personnes.firstname FROM societe left JOIN factures ON societe.idsociete = factures.idsociete LEFT JOIN personnes ON societe.idsociete = personnes.idsociete WHERE societe.idsociete = :id');
-
+    //detailsociete model
     $displayDetailsSocieties = $dbh->prepare('SELECT * FROM societe WHERE societe.idsociete = :id');
     $displayDetailsSocieties2 = $dbh->prepare('SELECT * FROM personnes WHERE personnes.idsociete = :id');
     $displayDetailsSocieties3 = $dbh->prepare('SELECT * FROM factures WHERE factures.idsociete = :id');
@@ -45,18 +48,9 @@ try {
       $displayDetailsSocieties3->execute();
     }
 
-
-    // $displayDetailsFactures->execute();
+    //detailfacture model
     if (isset($_GET['factures'])) {
-      $displayDetailsFactures = $dbh->prepare("SELECT * FROM factures LEFT JOIN type ON factures.idsociete = type.idsociete WHERE factures.idfactures = :idfactures");
-      $displayDetailsFactures->bindParam(':idfactures', $idfactures);
-
       $idfactures = $_GET['factures'];
-      $displayDetailsFactures->execute();
-
-      $displayDetailsFactures2 = $dbh->query("SELECT socialname, idsociete FROM societe");
-      $displayDetailsFactures3 = $dbh->query("SELECT idpersonnes, name, firstname FROM personnes");
-
       if (isset($_POST['update'])) {
         $updateDetailFacture = $dbh->prepare('UPDATE factures SET datefacture = :datefacture, prestationmotif = :prestationmotif, idsociete = :idsociete, idpersonnes = :idpersonnes WHERE idfactures = :idfactures');
         $updateDetailFacture->bindParam(':datefacture', $datefacture);
@@ -70,22 +64,39 @@ try {
         $idpersonnes = $_POST['idpersonnes'];
         $updateDetailFacture->execute();
       }
+      $displayDetailsFactures = $dbh->prepare("SELECT * FROM factures LEFT JOIN type ON factures.idsociete = type.idsociete WHERE factures.idfactures = :idfactures");
+      $displayDetailsFactures->bindParam(':idfactures', $idfactures);
+
+
+      $displayDetailsFactures->execute();
+
+      $displayDetailsFactures2 = $dbh->query("SELECT socialname, idsociete FROM societe");
+      $displayDetailsFactures3 = $dbh->query("SELECT idpersonnes, name, firstname FROM personnes");
+
+
     }
 
+    //detail contact model
     $displayDetailsPersonnes = $dbh->prepare('SELECT personnes.name, personnes.firstname, personnes.personnesphone, personnes.email FROM personnes WHERE personnes.idpersonnes = :idpersonnes');
-
     $displayDetailsPersonnes2 = $dbh->prepare('SELECT * FROM factures WHERE factures.idpersonnes = :idpersonnes');
-
     $displayDetailsPersonnes3 = $dbh->prepare('SELECT socialname, adresse FROM societe LEFT JOIN personnes ON societe.idsociete = personnes.idsociete WHERE personnes.idpersonnes = :idpersonnes');
 
     $displayDetailsPersonnes->bindParam(':idpersonnes', $idpersonnes);
     $displayDetailsPersonnes2->bindParam(':idpersonnes', $idpersonnes);
     $displayDetailsPersonnes3->bindParam(':idpersonnes', $idpersonnes);
+
     if (isset($_GET['annuaire'])) {
       $idpersonnes = $_GET['annuaire'];
       $displayDetailsPersonnes->execute();
       $displayDetailsPersonnes2->execute();
       $displayDetailsPersonnes3->execute();
+    }
+
+    if (isset($_POST['delete'])) {
+      $deleteFacture = $dbh->prepare("DELETE FROM factures WHERE idfactures = :idfactures");
+      $deleteFacture->bindParam(':idfactures', $idfactures);
+      $idfactures = $_POST['delete'];
+      $deleteFacture->execute();
     }
 
     } catch (PDOException $e) {
