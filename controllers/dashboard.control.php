9ea2e204658f6022a21385b61a179c5a46500c6a
gderuwez <gderuwez@gmail.com>
2018-08-31 10:22:02 +0200

    new merge

--- controllers/dashboard.control.php
+++ controllers/dashboard.control.php
@@ -3,6 +3,12 @@
 if (isset($_POST['delete'])) {
   delete("factures", "idfactures");
 }
+if (isset($_POST['delete2'])) {
+  delete("societe", "idsociete");
+}
+if (isset($_POST['delete3'])) {
+  delete("personnes", "idpersonnes");
+}
 require "models/dashboard.model.php";
 require "views/dashboard.view.php";
 
