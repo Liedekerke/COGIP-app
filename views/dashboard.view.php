833bc9fc1f9b713dd788c1caae0dbfb0c0b5b284
CharlotteTusset <charlotte.tusset@gmail.com>
2018-08-31 16:55:56 +0200

    Corrections front

--- views/dashboard.view.php
+++ views/dashboard.view.php
@@ -9,135 +9,103 @@
     </div>
   </section>
    <main>
-    <div class="tables">
-      <div class="box">
-        <h2 class="subtitle is-size-3 has-text-info has-text-weight-bold">Les dernières factures</h2>
-        <table>
-          <thead>
-            <th>id</th>
-            <th>date</th>
-            <th>motif prestations</th>
-            <th>nom société</th>
-            <th></th>
-          </thead>
-          <tbody>
-           <?php while ($donnee = $displayLatestFactures->fetch()) { ?>
-             <tr>
-               <td><a href="?page=detailfacture&factures=<?php echo $donnee['idfactures'] ?>"><?php echo $donnee['idfactures'] ?></a></td>
-               <td><?php echo $donnee['datefacture'] ?></td>
-               <td><?php echo $donnee['prestationmotif'] ?></td>
-               <td><a href="?page=detailsociete&societe=<?php echo $donnee['idsociete'] ?>"><?php echo $donnee['socialname'] ?></a></td>
-               <td>
-                 <form class="" action="" method="post">
-                   <input type="hidden" name="iddelete" value="<?php echo $donnee['idfactures'] ?>">
-                   <button class="button is-info" type="submit" name="delete"><i class="fas fa-trash-alt"></i></button>
-                 </form>
-               </td>
-             </tr>
-           <?php } ?>
-          </tbody>
-        </table>
-        <button type="button" name="create"> <a href="?page=newfacture">Nouvelle facture</a> </button>
-      </div>
-      <div class="box">
-        <h2 class="subtitle is-size-3 has-text-info has-text-weight-bold">Les dernières sociétés</h2>
-        <table>
-          <thead>
-            <th>id</th>
-            <th>nom société</th>
-            <th>adresse</th>
-            <th>pays</th>
-            <th>numéro tva</th>
-            <th>numéro téléphone</th>
-            <th></th>
-          </thead>
-          <tbody>
-            <?php while ($donneefourth = $displayLatestSocieties->fetch()) { ?>
+      <div class="tables">
+        <div class="box">
+          <h2 class="subtitle is-size-3 has-text-info has-text-weight-bold">Les dernières factures</h2>
+          <table>
+            <thead>
+              <th>id</th>
+              <th>date</th>
+              <th>motif prestations</th>
+              <th>nom société</th>
+              <th></th>
+            </thead>
+            <tbody>
+             <?php while ($donnee = $displayLatestFactures->fetch()) { ?>
+               <tr>
+                 <td><a href="?page=detailfacture&factures=<?php echo $donnee['idfactures'] ?>"><?php echo $donnee['idfactures'] ?></a></td>
+                 <td><?php echo $donnee['datefacture'] ?></td>
+                 <td><?php echo $donnee['prestationmotif'] ?></td>
+                 <td><a href="?page=detailsociete&societe=<?php echo $donnee['idsociete'] ?>"><?php echo $donnee['socialname'] ?></a></td>
+                 <td>
+                   <form class="" action="" method="post">
+                     <input type="hidden" name="iddelete" value="<?php echo $donnee['idfactures'] ?>">
+                     <button class="button is-info" type="submit" name="delete"><i class="fas fa-trash-alt"></i></button>
+                   </form>
+                 </td>
+               </tr>
+             <?php } ?>
+            </tbody>
+          </table>
+        </div>
+        <button class="button is-info create" type="button" name="create"> <a href="?page=newfacture">Nouvelle facture</a> </button>
+        <div class="box">
+          <h2 class="subtitle is-size-3 has-text-info has-text-weight-bold">Les dernières sociétés</h2>
+          <table>
+            <thead>
+              <th>id</th>
+              <th>nom société</th>
+              <th>adresse</th>
+              <th>pays</th>
+              <th>numéro tva</th>
+              <th>numéro téléphone</th>
+              <th></th>
+            </thead>
+            <tbody>
+              <?php while ($donneefourth = $displayLatestSocieties->fetch()) { ?>
+                <tr>
+                  <td><?php echo $donneefourth['idsociete'] ?></td>
+                  <td><a href="?page=detailsociete&societe=<?php echo $donneefourth['idsociete'] ?>"><?php echo $donneefourth['socialname'] ?></a></td>
+                  <td><?php echo $donneefourth['adresse'] ?></td>
+                  <td><?php echo $donneefourth['country'] ?></td>
+                  <td><?php echo $donneefourth['tvanumber'] ?></td>
+                  <td><?php echo $donneefourth['telephonesociete'] ?></td>
+                  <td>
+                    <form class="" action="" method="post">
+                     <input type="hidden" name="iddelete" value="<?php echo $donnee2['idsociete'] ?>">
+                     <button class="button is-info" type="submit" name="delete2"><i class="fas fa-trash-alt"></i></button>
+                   </form>
+                  </td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
+        <button class="button is-info create" type="button" name="create"><a href="?page=newsociete">Nouvelle société</a></button>
+        <div class="box">
+          <h2 class="subtitle is-size-3 has-text-info has-text-weight-bold">Les derniers contacts</h2>
+          <table>
+            <thead>
+              <th>id</th>
+              <th>nom</th>
+              <th>prénom</th>
+              <th>num téléphone</th>
+              <th>email</th>
+              <th>nom société</th>
+              <th></th>
+            </thead>
+            <tbody>
+              <?php while ($donneethird = $displayLatestPeople->fetch()) { ?>
               <tr>
-                <td><?php echo $donneefourth['idsociete'] ?></td>
-                <td><a href="?page=detailsociete&societe=<?php echo $donneefourth['idsociete'] ?>"><?php echo $donneefourth['socialname'] ?></a></td>
-                <td><?php echo $donneefourth['adresse'] ?></td>
-                <td><?php echo $donneefourth['country'] ?></td>
-                <td><?php echo $donneefourth['tvanumber'] ?></td>
-                <td><?php echo $donneefourth['telephonesociete'] ?></td>
+                <td><?php echo $donneethird['idpersonnes'] ?></td>
+                <td><a href="?page=detailcontact&annuaire=<?php echo $donneethird['idpersonnes'] ?>"><?php echo $donneethird['name'] ?></a></td>
+                <td><?php echo $donneethird['firstname'] ?></td>
+                <td><?php echo $donneethird['personnesphone'] ?></td>
+                <td><?php echo $donneethird['email'] ?></td>
+                <td><a href="?page=detailsociete&societe=<?php echo $donneethird['idsociete'] ?>"><?php echo $donneethird['socialname'] ?></a></td>
                 <td>
                   <form class="" action="" method="post">
-                   <input type="hidden" name="iddelete" value="<?php echo $donnee2['idsociete'] ?>">
-                   <button class="button is-info" type="submit" name="delete2"><i class="fas fa-trash-alt"></i></button>
-                 </form>
+                     <input type="hidden" name="iddelete" value="<?php echo $donnee3['idpersonnes'] ?>">
+                     <button class="button is-info" type="submit" name="delete3"><i class="fas fa-trash-alt"></i></button>
+                   </form>
                 </td>
               </tr>
-            <?php } ?>
-          </tbody>
-        </table>
-        <form>
-          <input class="button is-info" type="button" value="Nouvelle société" onclick="window.location.href='?page=newsociete'"/>
-        </form>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
+        <button class="button is-info create" type="button" name="create" value="create"><a href="?page=createcontact&createcontact">Nouveau client</a></button>
       </div>
-      <div class="box">
-        <h2 class="subtitle is-size-3 has-text-info has-text-weight-bold">Les derniers contacts</h2>
-        <table>
-          <thead>
-            <th>id</th>
-            <th>nom</th>
-            <th>prénom</th>
-            <th>numéro téléphone</th>
-            <th>email</th>
-            <th>nom société</th>
-            <th></th>
-          </thead>
-          <tbody>
-            <?php while ($donneethird = $displayLatestPeople->fetch()) { ?>
-            <tr>
-              <td><?php echo $donneethird['idpersonnes'] ?></td>
-              <td><a href="?page=detailcontact&annuaire=<?php echo $donneethird['idpersonnes'] ?>"><?php echo $donneethird['name'] ?></a></td>
-              <td><?php echo $donneethird['firstname'] ?></td>
-              <td><?php echo $donneethird['personnesphone'] ?></td>
-              <td><?php echo $donneethird['email'] ?></td>
-              <td><a href="?page=detailsociete&societe=<?php echo $donneethird['idsociete'] ?>"><?php echo $donneethird['socialname'] ?></a></td>
-              <td>
-                <form class="" action="" method="post">
-                 <input type="hidden" name="iddelete" value="<?php echo $donnee2['idsociete'] ?>">
-                 <button type="submit" name="delete2"><i class="fas fa-trash-alt"></i></button>
-               </form>
-              </td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <th>id</th>
-          <th>name</th>
-          <th>first name</th>
-          <th>phone number</th>
-          <th>email</th>
-          <th>nom de société</th>
-        </thead>
-        <tbody>
-          <?php while ($donneethird = $displayLatestPeople->fetch()) { ?>
-          <tr>
-            <td><?php echo $donneethird['idpersonnes'] ?></td>
-            <td><a href="?page=detailcontact&annuaire=<?php echo $donneethird['idpersonnes'] ?>"><?php echo $donneethird['name'] ?></a></td>
-            <td><?php echo $donneethird['firstname'] ?></td>
-            <td><?php echo $donneethird['personnesphone'] ?></td>
-            <td><?php echo $donneethird['email'] ?></td>
-            <td><a href="?page=detailsociete&societe=<?php echo $donneethird['idsociete'] ?>"><?php echo $donneethird['socialname'] ?></a></td>
-            <td>
-              <form class="" action="" method="post">
-                 <input type="hidden" name="iddelete" value="<?php echo $donnee3['idpersonnes'] ?>">
-                 <button class="button is-info" type="submit" name="delete3"><i class="fas fa-trash-alt"></i></button>
-               </form>
-            </td>
-
-          </tr>
-         <?php } ?>
-
-        </tbody>
-      </table>
-      </div>
-      <button type="button" name="create" value="create"><a href="?page=createcontact&createcontact">add</a></button>
-    </div>
   </main>
   <script type="text/javascript" src="./assets/js/main.js"></script>
   </body>
