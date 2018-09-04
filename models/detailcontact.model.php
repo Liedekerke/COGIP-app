449ab9841ab239c6bbc7d4bd5db074bf8430b5d8
gderuwez <gderuwez@gmail.com>
2018-09-04 09:37:52 +0200

    update societe

--- models/detailcontact.model.php
+++ models/detailcontact.model.php
@@ -1,7 +1,7 @@
 <?php
-$displayDetailsPersonnes = $dbh->prepare('SELECT personnes.name AS personnesName, personnes.firstname, personnes.personnesphone, personnes.email FROM personnes WHERE personnes.idpersonnes = :idpersonnes');
+$displayDetailsPersonnes = $dbh->prepare('SELECT personnes.name AS personnesName, personnes.firstname, personnes.personnesphone, personnes.email, personnes.idsociete FROM personnes WHERE personnes.idpersonnes = :idpersonnes');
 $displayDetailsPersonnes2 = $dbh->prepare('SELECT * FROM factures WHERE factures.idpersonnes = :idpersonnes');
-$displayDetailsPersonnes3 = $dbh->prepare('SELECT societe.idsociete, societe.socialname, societe.adresse FROM societe LEFT JOIN personnes ON societe.idsociete = personnes.idsociete WHERE personnes.idpersonnes = :idpersonnes');
+$displayDetailsPersonnes3 = $dbh->prepare('SELECT societe.idsociete, societe.socialname, societe.adresse FROM societe');
 
 $displayDetailsPersonnes->bindParam(':idpersonnes', $idpersonnes);
 $displayDetailsPersonnes2->bindParam(':idpersonnes', $idpersonnes);
@@ -14,6 +14,22 @@ if (isset($_GET['annuaire'])) {
   $displayDetailsPersonnes->execute();
   $displayDetailsPersonnes2->execute();
   $displayDetailsPersonnes3->execute();
+  $message = 'contact modifié avec succès.';
+}
+
+if (isset($_POST['update'])) {
+  $updateperson = $dbh->prepare('UPDATE personnes SET name = :name, firstname = :firstname, idsociete = :idsociete, personnesphone = :personnesphone, email = :email WHERE idpersonnes = :idpersonnes');
+  $updateperson->bindParam(':name', $name);
+  $updateperson->bindParam(':firstname', $firstname);
+  $updateperson->bindParam(':idsociete', $idsociete);
+  $updateperson->bindParam(':personnesphone', $personnesphone);
+  $updateperson->bindParam(':email', $email);
+  $name = $_POST['name'];
+  $firstname = $_POST['firstname'];
+  $idsociete = $_POST['idsociete'];
+  $personnesphone = $_POST['personnesphone'];
+  $email = $_POST['email'];
+  $updateperson->execute();
   $message = 'contact modifié avec succès.';
 }
 
--- views/detailcontact.view.php
+++ views/detailcontact.view.php
@@ -1,48 +1,41 @@
-   <table>
-      <thead>
-        <th>nom</th>
-        <th>prénom</th>
-        <th>téléphone</th>
-        <th>email</th>
-      </thead>
-      <tbody>
-        <?php while ($donnee = $displayDetailsPersonnes->fetch()) { ?>
-          <tr>
-            <td>
-              <input type="text" name="name" value="<?php echo $donnee['personnesName'] ?>">
-            </td>
-            <td>
-              <input type="text" name="firstname" value="<?php echo $donnee['firstname'] ?>">
-            </td>
-            <td>
-              <input type="number" name="personnesphone" value="<?php echo $donnee['personnesphone'] ?>">
-            </td>
-            <td>
-              <input type="email" name="email" value="<?php echo $donnee['email'] ?>">
-            </td>
-             <td><input type="submit" name="update" value="update"></td>
-          </tr>
-        <?php } ?>
-      </tbody>
-    </table>
+<form class="" action="" method="post">
+  <table>
+     <thead>
+       <th>nom</th>
+       <th>prénom</th>
+       <th>téléphone</th>
+       <th>email</th>
+       <th>social name</th>
+     </thead>
+     <tbody>
+       <tr>
+       <?php while ($donnee = $displayDetailsPersonnes->fetch()) { ?>
+           <td>
+             <input type="text" name="name" value="<?php echo $donnee['personnesName'] ?>">
+           </td>
+           <td>
+             <input type="text" name="firstname" value="<?php echo $donnee['firstname'] ?>">
+           </td>
+           <td>
+             <input type="number" name="personnesphone" value="<?php echo $donnee['personnesphone'] ?>">
+           </td>
+           <td>
+             <input type="email" name="email" value="<?php echo $donnee['email'] ?>">
+           </td>
+       <?php } ?>
+        <td>
+          <select class="" name="">
+           <?php while ($donnee3 = $displayDetailsPersonnes3->fetch()) { ?>
+             <option value="<?php echo $donnee3['idsociete'] ?>" <?php testselect($donnee3['idsociete'], $donnee['idsociete']) ?>><?php echo $donnee3['socialname'] ?></option>
+           <?php } ?>
+           </select>
+        </td>
+        <td><input type="submit" name="update" value="update"></td>
+       </tr>
+     </tbody>
+   </table>
+
     <?php echo $message; ?>
-
-    <table>
-      <thead>
-        <th>social name</th>
-        <th>adresse</th>
-      </thead>
-      <tbody>
-        <?php while ($donnee3 = $displayDetailsPersonnes3->fetch()) { ?>
-          <tr>
-            <td><a href="?page=detailsociete&societe=<?php echo $donnee3['idsociete'] ?>"><?php echo $donnee3['socialname'] ?></a>
-            </td>
-
-            <td><?php echo $donnee3['adresse']?></td>
-          </tr>
-        <?php } ?>
-      </tbody>
-    </table>
 
     <table>
       <thead>
