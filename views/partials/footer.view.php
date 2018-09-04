959cf05344407b571e80fb44faa78bd64d89d272
CharlotteTusset <charlotte.tusset@gmail.com>
2018-09-04 11:44:14 +0200

    footer

--- views/partials/footer.view.php
+++ views/partials/footer.view.php
@@ -1,3 +1,8 @@
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>Vive la COGIP!</p>
+	</div>
+</footer>
 <script type="text/javascript" src="./assets/js/main.js"></script>
 </body>
 </html>
