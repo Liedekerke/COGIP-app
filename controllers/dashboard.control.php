54677d90969ea45f7accecba4dc76491ef7139e7
gderuwez <gderuwez@gmail.com>
2018-10-03 10:59:03 +0200

    data update

--- controllers/dashboard.control.php
+++ controllers/dashboard.control.php
@@ -2,19 +2,37 @@
 session_start();
 require "controllers/partials/partial.control.php";
 sessionCheck();
-
-
+$errorDelFactures = '';
+$errorDelSociete = '';
+$errorDelPersonnes = '';
 if (isset($_POST['delete'])) {
-  delete("factures", "idfactures");
+  try {
+    delete("factures", "idfactures");
+  } catch (\Exception $e) {
+    $errorDelFactures = "la suppression ne c'est pas faite correctement";
+  }
 }
 if (isset($_POST['delete2'])) {
-  delete("societe", "idsociete");
+  try {
+    delete("societe", "idsociete");
+  } catch (\Exception $e) {
+    $errorDelSociete = "la suppression ne c'est pas faite correctement";
+  }
 }
 if (isset($_POST['delete3'])) {
-  delete("personnes", "idpersonnes");
+  try {
+    delete("personnes", "idpersonnes");
+  } catch (\Exception $e) {
+    $errorDelPersonnes = "la suppression ne c'est pas faite correctement";
+  }
 }
 require "models/dashboard.model.php";
 require "views/dashboard.view.php";
+
+
+
+
+
 
 
 
--- database/cgpi.sql
+++ database/cgpi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2018 at 01:54 PM
+-- Generation Time: Oct 03, 2018 at 10:58 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `cgpi`
 --
-CREATE DATABASE IF NOT EXISTS `cgpi` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `cgpi`;
 
 -- --------------------------------------------------------
 
@@ -30,12 +28,11 @@ USE `cgpi`;
 -- Table structure for table `devis`
 --
 
-CREATE TABLE IF NOT EXISTS `devis` (
-  `iddevis` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `devis` (
+  `iddevis` int(11) NOT NULL,
   `estimationtime` int(11) NOT NULL,
   `estimationprice` int(11) NOT NULL,
-  `datedevis` date NOT NULL,
-  PRIMARY KEY (`iddevis`)
+  `datedevis` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -44,16 +41,13 @@ CREATE TABLE IF NOT EXISTS `devis` (
 -- Table structure for table `factures`
 --
 
-CREATE TABLE IF NOT EXISTS `factures` (
-  `idfactures` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `factures` (
+  `idfactures` int(11) NOT NULL,
   `datefacture` date NOT NULL,
   `prestationmotif` varchar(45) NOT NULL,
   `idsociete` int(11) DEFAULT NULL,
-  `idpersonnes` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idfactures`),
-  KEY `factures_ibfk_1` (`idsociete`),
-  KEY `idpersonnes` (`idpersonnes`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+  `idpersonnes` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `factures`
@@ -81,13 +75,11 @@ INSERT INTO `factures` (`idfactures`, `datefacture`, `prestationmotif`, `idsocie
 -- Table structure for table `notecredit`
 --
 
-CREATE TABLE IF NOT EXISTS `notecredit` (
-  `idnotecredit` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `notecredit` (
+  `idnotecredit` int(11) NOT NULL,
   `reduction` int(11) NOT NULL,
   `datenotecredit` date NOT NULL,
-  `idfactures` int(11) NOT NULL,
-  PRIMARY KEY (`idnotecredit`),
-  KEY `notecredit_ibfk_1` (`idfactures`)
+  `idfactures` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -96,16 +88,14 @@ CREATE TABLE IF NOT EXISTS `notecredit` (
 -- Table structure for table `personnes`
 --
 
-CREATE TABLE IF NOT EXISTS `personnes` (
-  `idpersonnes` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `personnes` (
+  `idpersonnes` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `personnesphone` int(20) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `idsociete` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idpersonnes`),
-  KEY `personnes_ibfk_1` (`idsociete`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  `idsociete` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `personnes`
@@ -131,15 +121,14 @@ INSERT INTO `personnes` (`idpersonnes`, `name`, `firstname`, `personnesphone`, `
 -- Table structure for table `societe`
 --
 
-CREATE TABLE IF NOT EXISTS `societe` (
-  `idsociete` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `societe` (
+  `idsociete` int(11) NOT NULL,
   `socialname` varchar(45) NOT NULL,
   `adresse` varchar(45) NOT NULL,
   `country` varchar(45) NOT NULL,
   `tvanumber` int(3) NOT NULL,
-  `telephonesociete` int(20) NOT NULL,
-  PRIMARY KEY (`idsociete`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  `telephonesociete` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `societe`
@@ -160,14 +149,12 @@ INSERT INTO `societe` (`idsociete`, `socialname`, `adresse`, `country`, `tvanumb
 -- Table structure for table `type`
 --
 
-CREATE TABLE IF NOT EXISTS `type` (
-  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `type` (
+  `idtype` int(11) NOT NULL,
   `type` set('ASBL','SPRL','SA') NOT NULL,
   `relation` set('fournisseur','client') NOT NULL,
-  `idsociete` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idtype`),
-  KEY `idsociete` (`idsociete`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+  `idsociete` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `type`
@@ -181,6 +168,125 @@ INSERT INTO `type` (`idtype`, `type`, `relation`, `idsociete`) VALUES
 (5, 'SPRL', 'fournisseur', 5),
 (6, 'SA', 'client', 6),
 (22, 'SPRL', 'fournisseur', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `privilege` set('MODO','IDDQD','GUEST') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idusers`, `name`, `password`, `privilege`) VALUES
+(1, 'Jean-Christian', 'f1ae4a0bb3bd77e6eeec82949f50b334c409d4f7', 'IDDQD'),
+(3, 'guest', '35675e68f4b5af7b995d9205ad0fc43842f16450', 'GUEST'),
+(4, 'Muriel', 'ce61487ebde80c774d015152f38f945e73598af9', 'MODO');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `devis`
+--
+ALTER TABLE `devis`
+  ADD PRIMARY KEY (`iddevis`);
+
+--
+-- Indexes for table `factures`
+--
+ALTER TABLE `factures`
+  ADD PRIMARY KEY (`idfactures`),
+  ADD KEY `factures_ibfk_1` (`idsociete`),
+  ADD KEY `idpersonnes` (`idpersonnes`);
+
+--
+-- Indexes for table `notecredit`
+--
+ALTER TABLE `notecredit`
+  ADD PRIMARY KEY (`idnotecredit`),
+  ADD KEY `notecredit_ibfk_1` (`idfactures`);
+
+--
+-- Indexes for table `personnes`
+--
+ALTER TABLE `personnes`
+  ADD PRIMARY KEY (`idpersonnes`),
+  ADD KEY `personnes_ibfk_1` (`idsociete`);
+
+--
+-- Indexes for table `societe`
+--
+ALTER TABLE `societe`
+  ADD PRIMARY KEY (`idsociete`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`idtype`),
+  ADD KEY `idsociete` (`idsociete`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idusers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `devis`
+--
+ALTER TABLE `devis`
+  MODIFY `iddevis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `factures`
+--
+ALTER TABLE `factures`
+  MODIFY `idfactures` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `notecredit`
+--
+ALTER TABLE `notecredit`
+  MODIFY `idnotecredit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personnes`
+--
+ALTER TABLE `personnes`
+  MODIFY `idpersonnes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `societe`
+--
+ALTER TABLE `societe`
+  MODIFY `idsociete` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `idtype` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `idusers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
